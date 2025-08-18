cc4c3a64f69c33722e292b9664fbdcad87daa51b
Brian Durand <bbdurand@gmail.com>
2025-08-18 16:47:35 -0700

    configure rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
 require "fakeredis"
 
 require_relative "../lib/lumberjack_redis_device"
+
+RSpec.configure do |config|
+  config.warnings = true
+  config.disable_monkey_patching!
+  config.default_formatter = "doc" if config.files_to_run.one?
+  config.order = :random
+  Kernel.srand config.seed
+end
